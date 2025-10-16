a2cc6dbdbf538138b10a491c2078619e64e37ab5
Marta Torres <marta.torres@quantia.es>
2025-10-16 17:02:09 +0200

    Ignorar carpeta archivos

--- archivos/prueba.py
+++
@@ -1,7 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-print(os.getcwd())  # Muestra la ruta actual
-print(os.listdir()) # Muestra los archivos en la carpeta
-print(os.getenv("GOOGLE_API_KEY"))
